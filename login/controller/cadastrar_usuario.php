85cd3309172acf5c07004dd9e9de7822a760303c
Milena390 <milena.silva390@etec.sp.gov.br>
2025-08-12 12:03:43 -0300

    ok

--- login/controller/cadastrar_usuario.php
+++ login/controller/cadastrar_usuario.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
 
-require_once __DIR__ . '/../models/UsuarioDAO.php';
-require_once __DIR__ . '/../utils/Sanitizacao.php';
+require_once  '../models/UsuarioDAO.php';
+require_once  '../utils/Sanitizacao.php';
 
 $nome = Sanitizacao::sanitizar($_POST['nome']);
 $email = Sanitizacao::sanitizar($_POST['email']);
